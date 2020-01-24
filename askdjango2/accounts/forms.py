a8457f1dfa9258107aa428896e2186730837594e
DaeHwan Kim <mystletain@naver.com>
2020-01-25 00:04:08 +0900

    기본인증 ~13강

---
+++ askdjango2/accounts/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import validate_email
+
+
+class SignupForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].validators = [validate_email]
+        self.fields['username'].help_text = 'Enter Email Format.'
+        self.fields['username'].label = 'Email'
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = user.username
+        if commit:
+            user.save()
+        return user
+
+# class SignupForm(UserCreationForm):
+#     def clean_username(self):
+#         value = self.clean_data.get('username')
+#         if value:
+#             validate_email(value)
+#         return value
--- askdjango2/accounts/views.py
+++ askdjango2/accounts/views.py
@@ -1,24 +1,76 @@
-from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetConfirmView
+from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from accounts.forms import SignupForm
 
 
-def signup(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            auth_login(request, user)  # 로그인 처리
-            next_url = request.GET.get('next') or 'profile'
-            return redirect(next_url)
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {
-        'form': form,
-    })
+# def signup(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             auth_login(request, user)  # 로그인 처리
+#             next_url = request.GET.get('next') or 'profile'
+#             return redirect(next_url)
+#     else:
+#         form = UserCreationForm()
+#     return render(request, 'accounts/signup.html', {
+#         'form': form,
+#     })
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+
+signup = SignupView.as_view()
 
 
 @login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+
+class MyPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('profile')
+    template_name = 'accounts/password_change_form.html'
+
+    def form_valid(self, form):
+        messages.info(self.request, '암호 변경을 완료했습니다.')
+        return super().form_valid(form)
+
+
+class MyPasswordResetView(PasswordResetView):
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/password_reset_form.html'
+
+    def form_valid(self, form):
+        messages.info(self.request, '암호 변경 메일을 발송했습니다.')
+        return super().form_valid(form)
+
+
+class MyPasswordResetConfirmView(PasswordResetConfirmView):
+    success_url = reverse_lazy('login')
+    template_name = 'accounts/password_reset_confirm.html'
+
+    def form_valid(self, form):
+        messages.info(self.request, '암호 리셋을 완료했습니다.')
+        return super().form_valid(form)
